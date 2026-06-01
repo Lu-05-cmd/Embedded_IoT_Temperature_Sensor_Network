052cd901600b7847d124fb156e909db253cce64b
DTuan <doantuan1625@gmail.com>
2026-06-01 13:57:56 +0700

    feat: add WiFi driver

---
+++ src/wifi.c
@@ -0,0 +1,106 @@
+#include "wifi.h"
+#include "config.h"
+
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+
+#include "esp_log.h"
+
+#define WIFI_CONNECTED_BIT BIT0
+
+static EventGroupHandle_t wifi_event_group;
+static const char *TAG = "WIFI";
+
+static void wifi_event_handler(
+    void *arg,
+    esp_event_base_t event_base,
+    int32_t event_id,
+    void *event_data)
+{
+    if (event_base == WIFI_EVENT &&
+        event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+
+    else if (event_base == WIFI_EVENT &&
+             event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        ESP_LOGW(TAG, "Reconnect...");
+        esp_wifi_connect();
+    }
+
+    else if (event_base == IP_EVENT &&
+             event_id == IP_EVENT_STA_GOT_IP)
+    {
+        xEventGroupSetBits(
+            wifi_event_group,
+            WIFI_CONNECTED_BIT);
+
+        ESP_LOGI(TAG, "WiFi Connected");
+    }
+}
+
+void wifi_init(void)
+{
+    wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(
+        esp_event_loop_create_default());
+
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg =
+        WIFI_INIT_CONFIG_DEFAULT();
+
+    ESP_ERROR_CHECK(
+        esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(
+        esp_event_handler_register(
+            WIFI_EVENT,
+            ESP_EVENT_ANY_ID,
+            &wifi_event_handler,
+            NULL));
+
+    ESP_ERROR_CHECK(
+        esp_event_handler_register(
+            IP_EVENT,
+            IP_EVENT_STA_GOT_IP,
+            &wifi_event_handler,
+            NULL));
+
+    wifi_config_t wifi_config = {0};
+
+    strcpy(
+        (char*)wifi_config.sta.ssid,
+        WIFI_SSID);
+
+    strcpy(
+        (char*)wifi_config.sta.password,
+        WIFI_PASS);
+
+    ESP_ERROR_CHECK(
+        esp_wifi_set_mode(WIFI_MODE_STA));
+
+    ESP_ERROR_CHECK(
+        esp_wifi_set_config(
+            WIFI_IF_STA,
+            &wifi_config));
+
+    ESP_ERROR_CHECK(
+        esp_wifi_start());
+
+    xEventGroupWaitBits(
+        wifi_event_group,
+        WIFI_CONNECTED_BIT,
+        pdFALSE,
+        pdTRUE,
+        portMAX_DELAY);
+}
